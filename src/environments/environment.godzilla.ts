3ba958d8e4e7191df58e97ab99454060e122a551
鈴木泰斗 <suzukitaito@suzukitaitos-MacBook-Pro.local>
2019-09-17 01:01:43 +0900

    save

--- src/environments/environment.godzilla.ts
+++ src/environments/environment.godzilla.ts
@@ -1,3 +1,11 @@
+import { Environment } from './common';
 export const environment = {
-  production: true
-};
+  production: true,
+  api: {
+    protocol: 'https',
+    hostname: 'storage.googleapis.com',
+    port: 4443,
+    ignorePort: true,
+  },
+  projectId: 'autr-godzilla',
+} as Environment;
